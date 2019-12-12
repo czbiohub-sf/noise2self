f3aa8220ea74da52e5f463f772e856281b5b4db0
Joshua Batson <joshua.batson@czbiohub.org>
2019-12-11 16:26:37 -0800

    Break out kernel predictor from DnCNN

--- models/dncnn.py
+++ models/dncnn.py
@@ -4,19 +4,18 @@ import torch
 import math
 
 class DnCNN(nn.Module):
-    def __init__(self, channels, num_of_layers=17):
+    def __init__(self, in_channels, out_channels, depth=17, features = 64):
         super(DnCNN, self).__init__()
         kernel_size = 3
         padding = 1
-        features = 64
         layers = []
-        layers.append(nn.Conv2d(in_channels=channels, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
+        layers.append(nn.Conv2d(in_channels=in_channels, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
         layers.append(nn.ReLU(inplace=True))
-        for _ in range(num_of_layers - 2):
+        for _ in range(depth - 2):
             layers.append(nn.Conv2d(in_channels=features, out_channels=features, kernel_size=kernel_size, padding=padding, bias=False))
             layers.append(nn.BatchNorm2d(features))
             layers.append(nn.ReLU(inplace=True))
-        layers.append(nn.Conv2d(in_channels=features, out_channels=channels, kernel_size=kernel_size, padding=padding, bias=False))
+        layers.append(nn.Conv2d(in_channels=features, out_channels=out_channels, kernel_size=kernel_size, padding=padding, bias=False))
         self.dncnn = nn.Sequential(*layers)
 
     def forward(self, x):
